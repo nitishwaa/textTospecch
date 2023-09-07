8a2e48af2779c95ea6117700ac05eac0419f4862
nitishwa <84021138+nitishwaa@users.noreply.github.com>
2023-09-07 12:34:18 +0530

    Update textToSPeech.py

--- textToSPeech.py
+++ textToSPeech.py
@@ -5,7 +5,7 @@ import base64
 def text2Speech(data):
     my_text = data
     tts = gTTS(text=my_text, lang='en', slow=False)
-    tts.save('converted-file.mp3')  # save file as ... (here saving as mp3)
-    with open("converted-file.mp3", "rb") as file:
+    tts.save('/tmp/converted-file.mp3')  # save file as ... (here saving as mp3)
+    with open("/tmp/converted-file.mp3", "rb") as file:
         my_string = base64.b64encode(file.read())
     return my_string
